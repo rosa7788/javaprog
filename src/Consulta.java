0bf6e929d6c75977b3d3d1b3245f6a29ce08cca0
rosa7788 <rosa05meneghetti@gmail.com>
2026-03-23 15:16:33 -0300

    clinica

---
+++ src/Consulta.java
@@ -0,0 +1,44 @@
+
+
+import java.time.LocalDateTime;
+
+public class Consulta {
+    private LocalDateTime data;
+    private double valorDaConsulta;
+    private Medico medico;
+    private Paciente paciente;
+
+    public Consulta(LocalDateTime data, double valorDaConsulta, Medico medico, Paciente paciente) {
+        this.data = data;
+        this.valorDaConsulta = valorDaConsulta;
+        this.medico = medico;
+        this.paciente = paciente;
+    }
+
+    // Getters
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public double getValorDaConsulta() {
+        return valorDaConsulta;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    @Override
+    public String toString() {
+        return "Consulta{" +
+                "data=" + data +
+                ", valorDaConsulta=" + valorDaConsulta +
+                ", medico=" + medico.getNome() +
+                ", paciente=" + paciente.getNome() +
+                '}';
+    }
+}
